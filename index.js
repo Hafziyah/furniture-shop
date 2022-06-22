b560f1635e356bded65d6735a5a4b466447780ce
Hafsat Ajia-Egbeyemi <hafsatajia@gmail.com>
2022-06-22 14:10:52 +0100

    More commitments

--- index.js
+++ index.js
@@ -1,31 +1,19 @@
-let homePage = "/index.html"
-let "forwardButton" = document.getElementById("forward-btn")
-console.log(forwardButton)
-
-const arrayOfImages = [
-    "hafsat", "paul", "mayowa"
-]
 
 
 
 
-forwardButton(index){
-    const result = arrayOfImages[index]
-    console.log(result)
-}
 
-forwardButton(0)
-
-
-// let presentPage = '#'
-
-
-// const handleNavigateForward = () =>{
-//     if(presentPage === '#'){
-//         presentPage === "manufacture/index.html"
-//     }else if( presentPage === 'manufacture/index.html'){
-//         presentPage === 'globally/index.html'
-//     }
+// function back(){
+//     document.getElementById("backgroundPic").style.backgroundImage = "url('./img/desktop-image-hero-1.jpg')";
+//     document.getElementById('textSection__discover').innerText="We provide unmatched quality, comfort, and style for property owners across the country. Our experts combine form and function in bringing your vision to life. Create a room in your own style with our collection and make your property a reflection of you and what you love.";
+//     console.log("back")
 // }
 
+// function forward(){
+//      document.getElementById("backgroundPicA").style.backgroundImage = "url('./img/desktop-image-hero-2.jpg')";
+//      document.getElementById('textSection__availability').innerText= "With stores all over the world, it's easy for you to find furniture for your home or place of business. Locally, we're in most major cities throughout the country. Find the branch nearest you using our store locator. Any questions? Don't hesitate to contact us today.";
+//      console.log("forward")
+// }
 
+// var imageFirst = document.querySelector("#backgroundPic");
+// console.log(imageFirst)
